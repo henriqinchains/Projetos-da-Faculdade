6ba66d898b6737de7b17909e920a21036baef7da
Meira <henriquemeira321@gmail.com>
2026-04-13 22:04:13 -0300

    Refactor contact input and display logic

--- ProjetoContato.cpp
+++ ProjetoContato.cpp
@@ -134,131 +134,39 @@ int main(int argc, char** argv)
 {
 	string input;
 	int dia, mes, ano;
+	Contato contato[5];
 	
-
-		
-	Contato um;
-	Contato dois;
-	Contato tres;
-	Contato quatro;
-	Contato cinco;
-	
+	for(int i=0; i<5 ;i++){
+    cout << "Digite o nome do contato: " << endl;
+	cin >> input;
+	contato[i].setNome(input);
 	cout << "Digite o email do contato: " << endl;
 	cin >> input;
-	um.setEmail(input);
-	cout << "Digite o nome do contato: " << endl;
-	cin >> input;
-	um.setNome(input);
+	contato[i].setEmail(input);
 	cout<< "Digite o telefone do contato: " << endl;
 	cin >> input;
-	um.setTelefone(input);
+	contato[i].setTelefone(input);
 	cout << "Digite a data de nascimento: (dd/mm/aaaa/)" << endl;
 	cin >> dia >> mes >> ano;
-	Data nasc1(dia, mes, ano);
-	um.setDtNasc(nasc1);
+	contato[i].setDtNasc(Data(dia, mes, ano));
 	cin.clear();
-	
-	cout << "Digite o email do segundo contato: " << endl;
-	cin >> input;
-	dois.setEmail(input);
-	cout << "Digite o nome do segundo contato: " << endl;
-	cin >> input;
-	dois.setNome(input);
-	cout<< "Digite o telefone do segundo contato: " << endl;
-	cin >> input;
-	dois.setTelefone(input);
-	cout << "Digite a data de nascimento do segundo contato: (dd/mm/aaaa/)" << endl;
-	cin >> dia >> mes >> ano;
-	Data nasc2(dia, mes, ano);
-	dois.setDtNasc(nasc2);
-	
-	cout << "Digite o email do terceiro contato: " << endl;
-	cin >> input;
-	tres.setEmail(input);
-	cout << "Digite o nome do terceiro contato: " << endl;
-	cin >> input;
-	tres.setNome(input);
-	cout<< "Digite o telefone do terceiro contato: " << endl;
-	cin >> input;
-	tres.setTelefone(input);
-	cout << "Digite a data de nascimento do terceiro contato: (dd/mm/aaaa/)" << endl;
-	cin >> dia >> mes >> ano;
-	Data nasc3(dia, mes, ano);
-	tres.setDtNasc(nasc3);
-	cin.clear();
-	
-	cout << "Digite o email do quarto contato: " << endl;
-	cin >> input;
-	quatro.setEmail(input);
-	cout << "Digite o nome do quarto contato: " << endl;
-	cin >> input;
-	quatro.setNome(input);
-	cout<< "Digite o telefone do quarto contato: " << endl;
-	cin >> input;
-	quatro.setTelefone(input);
-	cout << "Digite a data de nascimento do quarto contato: (dd/mm/aaaa/)" << endl;
-	cin >> dia >> mes >> ano;
-	Data nasc4(dia, mes, ano);
-	quatro.setDtNasc(nasc4);
-	
-	cout << "Digite o email do quinto contato: " << endl;
-	cin >> input;
-	cinco.setEmail(input);
-	cout << "Digite o nome do quinto contato: " << endl;
-	cin >> input;
-	cinco.setNome(input);
-	cout<< "Digite o telefone do quinto contato: " << endl;
-	cin >> input;
-	cinco.setTelefone(input);
-	cout << "Digite a data de nascimento do quinto contato: (dd/mm/aaaa/)" << endl;
-	cin >> dia >> mes >> ano;
-	Data nasc5(dia, mes, ano);
-	cinco.setDtNasc(nasc5);
+	}
+
 	
 	
 	cout << " _____________________________________  " << endl;
 	cout << "|      Contatos cadastrados: 5        |  " << endl;					
 	cout << "|-------------------------------------|  " << endl;
     cout << "|                                     | " << endl;
-	cout << "|_____________________________________| " << endl;
-	cout << "| Nome: " << um.getNome() << "        " << endl;
-	cout << "|-------------------------------------|" << endl;
-	cout << "|Email: " << um.getEmail() << endl;
-    cout << "|Telefone: " << um.getTelefone() << endl;
-    cout << "|Nascimento: " << um.getDtNasc().getDia() << "/" << um.getDtNasc().getMes() << "/" << um.getDtNasc().getAno() << endl;
+    for(int i=0; i<5 ;i++){
+    cout << "|_____________________________________| " << endl;
+	cout << "| Nome: " << contato[i].getNome() << "        " << endl;
+	cout << "|                                     |" << endl;
+	cout << "| Email: " << contato[i].getEmail() << endl;
+    cout << "| Telefone: " << contato[i].getTelefone() << endl;
+    cout << "| Nascimento: " << contato[i].getDtNasc().getDia() << "/" << contato[i].getDtNasc().getMes() << "/" << contato[i].getDtNasc().getAno() << endl;
 	cout << " =====================================|" << endl;
-    cout << "|                                     | " << endl;
-    cout << "|_____________________________________| " << endl;
-	cout << "| Nome: " << dois.getNome() << "        " << endl;
-	cout << "|-------------------------------------|" << endl;
-	cout << "|Email: " << dois.getEmail() << endl;
-    cout << "|Telefone: " << dois.getTelefone() << endl;
-    cout << "|Nascimento: " << dois.getDtNasc().getDia() << "/" << dois.getDtNasc().getMes() << "/" << dois.getDtNasc().getAno() << endl;
-	cout << " ===================================== |" << endl;
-    cout << "|                                     | " << endl;
-    cout << "|_____________________________________| " << endl;
-	cout << "| Nome: " << tres.getNome() << "        " << endl;
-	cout << "|-------------------------------------|" << endl;
-	cout << "|Email: " << tres.getEmail() << endl;
-    cout << "|Telefone: " << tres.getTelefone() << endl;
-    cout << "|Nascimento: " << tres.getDtNasc().getDia() << "/" << tres.getDtNasc().getMes() << "/" << tres.getDtNasc().getAno() << endl;
-	cout << " ===================================== |" << endl;
-    cout << "|                                     | " << endl;
-    cout << "|_____________________________________| " << endl;
-	cout << "| Nome: " << quatro.getNome() << "        " << endl;
-	cout << "|-------------------------------------|" << endl;
-	cout << "|Email: " << quatro.getEmail() << endl;
-    cout << "|Telefone: " << quatro.getTelefone() << endl;
-    cout << "|Nascimento: " << quatro.getDtNasc().getDia() << "/" << quatro.getDtNasc().getMes() << "/" << quatro.getDtNasc().getAno() << endl;
-	cout << " ===================================== |" << endl;
-    cout << "|                                     | " << endl;
-    cout << "|_____________________________________| " << endl;
-	cout << "| Nome: " << cinco.getNome() << "        " << endl;
-	cout << "|-------------------------------------|" << endl;
-	cout << "|Email: " << cinco.getEmail() << endl;
-    cout << "|Telefone: " << cinco.getTelefone() << endl;
-    cout << "|Nascimento: " << cinco.getDtNasc().getDia() << "/" << cinco.getDtNasc().getMes() << "/" << cinco.getDtNasc().getAno() << endl;
-	cout << " ===================================== |" << endl;
+}
 
 
 	
